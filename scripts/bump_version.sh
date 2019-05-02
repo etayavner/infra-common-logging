99db51619ce38b869ac6a2e1e6dcf139b16e3451
iavner <iavner@cisco.com>
2019-05-02 11:27:49 +0300

    test

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -9,9 +9,9 @@ json=$dir/../../node_modules/json/lib/json.js
 current_version=$($json "version" < package.json)
 previous_version=$(git show HEAD:package.json | $json version)
 if [[ "$previous_version" != "$current_version" ]]; then
-   exit 0
+   exit 1
 fi
 new_version=$(echo $current_version | perl -ne '@p=split(/\./); $p[-1]++; print join(".",@p)')
 echo new_version=$new_version
 $json -I -f package.json -e this.version=\"$new_version\"
-exit 1
+exit 0
