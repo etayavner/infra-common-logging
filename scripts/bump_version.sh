6ba96579091d73cd8f1fac6a2b84ed3a722a45c9
iavner <iavner@cisco.com>
2019-05-02 12:05:11 +0300

    Commit

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -4,14 +4,18 @@
 
 # this can be used as a self-correcting pre-commit check
 
-dir=$(dirname $0)
-json=$dir/../../node_modules/json/lib/json.js
+json=node_modules/json/lib/json.js
 current_version=$($json "version" < package.json)
-previous_version=$(git show HEAD:package.json | $json version)
-if [[ "$previous_version" != "$current_version" ]]; then
-   exit 1
+echo current_version=$current_version
+repository_version=$(git show HEAD:package.json | $json version)
+echo repository_version=$repository_version
+if [[ "$repository_version" != "$current_version" ]]; then
+   exit 0
 fi
 new_version=$(echo $current_version | perl -ne '@p=split(/\./); $p[-1]++; print join(".",@p)')
 echo new_version=$new_version
 $json -I -f package.json -e this.version=\"$new_version\"
-exit 0
+
+git commit package.json -m "Updating package version to $new_version"
+
+git push origin master
