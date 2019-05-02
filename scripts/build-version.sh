5731b6bd64ce7bbab8e87012c3021068f4622f7e
iavner <iavner@cisco.com>
2019-05-02 14:06:28 +0300

    Work

--- scripts/build-version.sh
+++ scripts/build-version.sh
@@ -3,18 +3,20 @@
 ##########################################
 # Check that all files commited versions #
 ##########################################
-git diff-index --quiet HEAD -- || exit "Uncommitted changes found! please commit + push all changes before publishing"
+#git diff-index --quiet HEAD -- || exit "Uncommitted changes found! please commit + push all changes before publishing"
 
 ###################
 # Verify versions #
 ###################
 
+# Verify against repository
 json=node_modules/json/lib/json.js
+packageName=$($json "name" < package.json)
 current_version=$($json "version" < package.json)
 repository_version=$(git show HEAD:package.json | $json version)
 if [[ "$repository_version" != "$current_version" ]]; then
    echo "Conflict between versions: echo current_version=$current_version, echo repository_version=$repository_version"
-   echo "Please verify what you doing!"
+   echo "Please verify what you doing"
    exit 1
 fi
 
@@ -24,6 +26,19 @@ fi
 new_version=$(echo $current_version | perl -ne '@p=split(/\./); $p[-1]++; print join(".",@p)')
 echo new_version=$new_version
 $json -I -f package.json -e this.version=\"$new_version\"
+
+
+###################
+# Verify versions #
+###################
+
+# Verify against NPM registry
+test -z "$(npm info ${packageName}@${new_version})"
+if [[ ! $? -eq 0 ]]
+then
+    exit "$packageName@$new_version is already published in npm registry"
+fi
+
 
 ###################
 # Commit changes  #
